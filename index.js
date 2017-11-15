09e936fc3cf242ee43bb56211605a2d28badd36d
David Kim <mail.ddk@gmail.com>
2017-11-15 02:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function nameToAttributes (list) {
   });
 }
 
-function attributesToPhrase(){
-
+function attributesToPhrase(drivers){
+  return drivers.map(function (driver){
+    return `${driver.name} is from ${driver.hometown}`
+  });
 }
 
-//  * `nameToAttributes` - this function takes an array of drivers with their first and last name separated
-// by a space, and returns an array of JavaScript objects with `firstName` and `lastName` attributes.
 //  * `attributesToPhrase` - this function takes an array of drivers as JavaScript objects and returns a
 // string saying `"<NAME OF DRIVER> is from <HOMETOWN>"` for each JavaScript object in the array.
 // Note that between the `<` and `>` we are looking for the value stored in the `name` and `hometown`
