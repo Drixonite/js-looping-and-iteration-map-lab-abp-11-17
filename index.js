81c50ad01f8370362590459453f99fac47bfcdb9
David Kim <mail.ddk@gmail.com>
2017-11-15 02:50:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 // // Code your solution in this file.
 
-
-// function lowerCaseDrivers (list) {
-//   return list.map(function (driver) {
-//     return driver.toLowerCase();
-//   });
-// }
 function lowerCaseDrivers (list) {
   return list.map(function(driver){
     return driver.toLowerCase();
   });
 }
 
-function nameToAttributes(){
-
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return { firstName: driverFirst, lastName: driverLast };
+  });
 }
 
 function attributesToPhrase(){
 
 }
-// You'll be writing three functions:
-//  * `lowerCaseDrivers` - this function takes an array of drivers, and returns an array of the drivers names
-// in lowercase.
+
 //  * `nameToAttributes` - this function takes an array of drivers with their first and last name separated
 // by a space, and returns an array of JavaScript objects with `firstName` and `lastName` attributes.
 //  * `attributesToPhrase` - this function takes an array of drivers as JavaScript objects and returns a
