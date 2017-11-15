95610352364f87f3f17f223b67e1c0126385a04e
David Kim <mail.ddk@gmail.com>
2017-11-15 02:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // // Code your solution in this file.
 
 
+// function lowerCaseDrivers (list) {
+//   return list.map(function (driver) {
+//     return driver.toLowerCase();
+//   });
+// }
 function lowerCaseDrivers (list) {
-  return list.map(function (driver) {
+  return let list.map(function(driver){
     return driver.toLowerCase();
   });
 }
-// function lowerCaseDrivers (list) {
-//   return let list.map(function(driver){
-//     return driver.toLowerCase();
-//   })
-// }
 
 function nameToAttributes(){
 
